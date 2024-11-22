28c018a0c831f9c4d817d2ac02535d6ee5aafd6d
bonutt <bonuccellibr@gmail.com>
2024-11-22 12:58:16 -0300

    Create main.py

---
+++ main.py
@@ -0,0 +1,31 @@
+#RM560950 LUIGI PEREIRA BONUCCELLI
+def media(consumos): #FUNÇAO QUE CALCULA A MEDIA DO CONSUMO DE ENERGIA
+    return sum(consumos) / len(consumos)
+
+def energia(consumo): #FUNÇAO QUE PASSA FEEDBACKS DO USO DE ENERGIA
+    if consumo > 20:
+        return "Diminua o uso de energia e considere usar mais energia solar."
+    elif consumo > 10:
+        return "Você está no caminho certo"
+    else:
+        return "Ótimo uso de energia!"
+
+consumos = [] #LISTA
+
+print("Digite o consumo de energia em kwh para cada dia da semana:")
+
+
+for i in range(7): #COLETA DOS DADOS FORNECIDOS
+    dia = input(f"Dia {i+1}: ")
+    consumos.append(int(dia))
+
+mediaConsumo = media(consumos)
+print(f"\nMédia de consumo de energia durante a semana: {mediaConsumo:.2f} kWh")
+print(energia(mediaConsumo))
+print("\nConsumo diário de energia:")
+
+i = 1
+#ANALISE INDIVIDUAL DOS DIAS DA SEMANA
+for consumo in consumos:
+    print(f"Dia {i}: {consumo} kwh - {energia(consumo)}")
+    i += 1
